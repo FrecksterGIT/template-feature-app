c5eaaa4dbb132ad7fc3da011448794aaac71dffe
Sven Müller <sven.mueller@breathless-code.de>
2020-05-28 04:56:08 +0200

    build: remove audi-core-css from dist

--- src/demo/demo.tsx
+++ src/demo/demo.tsx
@@ -1,8 +1,11 @@
 import {FeatureHubReact, React, renderDemo} from '@das-buro-am-draht/feature-app-demo-integrator';
+import {version as coreCssVersion} from '@das-buro-am-draht/audi-core-css/package.json';
+
+const coreCSSUrl = 'https://assets.audi.com/audi-core-css/' + coreCssVersion + '/audi-core-css.min.css';
 
 renderDemo(
   <>
-    <link rel={'stylesheet'} href={'audi-core-css/audi-core-css.min.css'}/>
+    <link rel={'stylesheet'} href={coreCSSUrl}/>
     <FeatureHubReact.FeatureAppLoader
       featureAppId="demo"
       baseUrl=".."
